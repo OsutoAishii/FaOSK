0e2cf13f287d45bdfc0adc6359099b0020a59812
FirstArchve <first_archve@outlook.com>
2021-02-08 13:45:18 +0800

    Add keybroad support and organize the code

--- kernel/device/int.c
+++ kernel/device/int.c
@@ -7,6 +7,7 @@
 #include "ports.h"
 #include "types.h"
 #include "easyio.h"
+#include "keyboard.h"
 
 //定时器中断函数
 void timer_call(pt_regs *regs)
@@ -35,6 +36,7 @@ void timer_call(pt_regs *regs)
 		cursor=set_cursor(0,8);
 		printf("%.2d:%.2d:%.2d\n",hour,min,sec);
         set_cursor(cursor.x,cursor.y);
+		printf(" \b");
 	}
 }
 
@@ -47,7 +49,8 @@ int64 get_total_second()
 //键盘中断函数
 void keyboard_call(pt_regs *regs)
 {
-	unsigned char data;
-	data=inb(0x0060);
-	printf("%02x",data);
+	unsigned char be_data,af_data;
+	be_data=inb(0x0060);
+	af_data = data_hander(be_data);
+	printf("%c",af_data);
 }
---
+++ kernel/device/keyboard.h
@@ -0,0 +1,13 @@
+// Filename:  keyboard.h
+// Description:  键盘处理函数
+// Compiler:  gcc
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+static int shift;
+static int capslock;
+
+unsigned char data_hander(unsigned char be_data);
+
+#endif
